ebce00651d90c25c67f6230a26131e729b9de4ae
dumplingggg <81600369+dumplingggg@users.noreply.github.com>
2024-10-24 20:08:25 -0600

    countCities fail

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -58,4 +58,7 @@ public class CustomList extends ArrayAdapter<City> {
     public void delete(City city) {
         cities.remove(city);
     }
+    public int countCities() {
+        return cities.size();
+    }
 }
